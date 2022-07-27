50114152a2d5536b10464a6784abe00349463503
Code with Divyam <divyambansal17@gmail.com>
2022-07-27 19:40:44 +0530

    bad css added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Loginform from './Components/Forms/Loginform';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Loginform/>
     </div>
   );
 }
---
+++ src/Components/Forms/Loginform.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Loginform.css';
+
+function Loginform() {
+  return (<>
+   <div className='img_logo1'>
+ <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwFknsvL1uwnMURYkkC4c5H8GvsSj8QSjV0g&usqp=CAU.png" />
+ </div>
+ <div className='maindiv'>
+    <div className='loginform__div'>
+        <h1>Sign-In</h1>
+        <div>
+            <label>Email or mobile phone number</label><br/>
+            <input type ="text" className="input__field"/> 
+            <button className='btnclass'>Continue</button>
+         </div>     
+    </div>
+</div>
+    </>
+  )
+}
+
+export default Loginform
