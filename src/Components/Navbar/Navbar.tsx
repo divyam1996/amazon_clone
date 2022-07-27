c2eda194768b48196ded410d9d66d52f38e72e5c
sumitpant <pantsumit852@gmail.com>
2022-07-27 17:24:43 +0530

    input box added

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,12 +1,22 @@
 
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
 import './Navbar.css';
 
 function Navbar() {
   return (
     <div className="navbar__div">
      <img src="https://www.pngmart.com/files/Amazon-Logo-Download-PNG-Image.png" className="img_logo"/>
-
+      <div className="input__div">
+        <input type ="text" className="input__field"/> 
+        <div className="search_icondiv">
+        <SearchIcon/>
+        </div>
+      </div>
+    <div>Country drop down</div>
+    <div>Hello</div>
+    <div>Returns</div>
+    <div>CART</div>
         
    
     </div>
